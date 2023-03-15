b2f108dc9b92cb53399d3ad576175b624849405c
Touko Lonka <touko.lonka@gmail.com>
2023-03-15 13:00:23 +0200

    MongoDB docker configuration

--- server/utils/config.js
+++ server/utils/config.js
@@ -2,7 +2,10 @@
 require('dotenv').config({ path : '../project.env' });
 
 const { PORT } = process.env;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI_PROD
+    : process.env.MONGODB_URI_DEV;
 
 module.exports = {
   MONGODB_URI,
