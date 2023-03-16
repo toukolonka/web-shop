882602aab7a9ac2d067b8515708f21fc8c3a52d0
Touko Lonka <touko.lonka@gmail.com>
2023-03-16 17:50:37 +0200

    Log tables for all lighthouse tests

--- test-playwright/tests/pages.spec.js
+++ test-playwright/tests/pages.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('@playwright/test');
-const { lighthouseTest } = require('./utils');
+const { lighthouseTest, getReportsConfiguration, printTable } = require('./utils');
 const { playAudit } = require('playwright-lighthouse');
 
 const thresholds = {
@@ -10,132 +10,152 @@ const thresholds = {
   pwa: 10,
 };
 
-const reports = (prefix) => {
-  return {
-    formats: {
-      html: true,
-    },
-    name: `${prefix}-page`,
-    directory: 'reports',
-  };
-};
-
-lighthouseTest.describe('pages', () => {
-  lighthouseTest('home page', async ({ page, port }) => {
-    await page.goto('/');
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('home'),
-    });
-  });
-
-  lighthouseTest('products page', async ({ page, port }) => {
-    await page.goto('/products');
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('products'),
-    });
-  });
-
-  lighthouseTest('product page', async ({ page, port }) => {
-    await page.goto('/products');
-
-    await page.getByTestId('productCard100').click();
-
-    await expect((page).getByText(/product/)).toBeVisible();
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('product'),
-    });
-  });
-
-  lighthouseTest('cart page', async ({ page, port }) => {
-    await page.goto('/products');
-
-    await page.getByTestId('productCard100').click();
-
-    await expect((page).getByText(/product/)).toBeVisible();
-
-    await page.getByTestId('addToCartButton').click();
-
-    await page.goto('/cart');
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('cart'),
-    });
-  });
-
-  lighthouseTest('order page', async ({ page, port }) => {
-    await page.goto('/products');
-
-    await page.getByTestId('productCard100').click();
-
-    await expect((page).getByText(/product/)).toBeVisible();
-
-    await page.getByTestId('addToCartButton').click();
-
-    await page.goto('/cart');
-
-    await page.getByTestId('firstNameInput').fill('Touko');
-    await page.getByTestId('lastNameInput').fill('Lonka');
-    await page.getByTestId('addressInput').fill('Address 123');
-
-    await page.getByTestId('placeOrderButton').click();
-
-    await page.getByTestId('confirmButton').click();
-
-    await expect((page).getByText(/Order on/)).toBeVisible();
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('order'),
-    });
-  });
-
-  lighthouseTest('orders page', async ({ page, port }) => {
-    await page.goto('/products');
-
-    await page.getByTestId('productCard100').click();
-
-    await expect((page).getByText(/product/)).toBeVisible();
-
-    await page.getByTestId('addToCartButton').click();
-
-    await page.goto('/cart');
-
-    await page.getByTestId('firstNameInput').fill('Touko');
-    await page.getByTestId('lastNameInput').fill('Lonka');
-    await page.getByTestId('addressInput').fill('Address 123');
-
-    await page.getByTestId('placeOrderButton').click();
-
-    await page.getByTestId('confirmButton').click();
-
-    await expect((page).getByText(/Order on/)).toBeVisible();
-
-    await page.goto('/orders');
-
-    await expect((page).getByText(/Order on/)).toBeVisible();
-
-    await playAudit({
-      page,
-      port,
-      thresholds,
-      reports: reports('orders'),
-    });
-  });
+lighthouseTest.afterAll(() => {
+  printTable();
 });
+
+testSuites(['react']);
+
+function testSuites(names) {
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} home page ${i}`, async ({ page, port }) => {
+        await page.goto('/');
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-home-${i}`),
+        });
+      })
+    )
+  );
+
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} products page ${i}`, async ({ page, port }) => {
+        await page.goto('/products');
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-products-${i}`),
+        });
+      })
+    )
+  );
+
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} product page ${i}`, async ({ page, port }) => {
+        await page.goto('/products');
+
+        await page.getByTestId('productCard100').click();
+
+        await expect((page).getByText(/product/)).toBeVisible();
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-product-${i}`),
+        });
+      })
+    )
+  );
+
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} cart page ${i}`, async ({ page, port }) => {
+        await page.goto('/products');
+
+        await page.getByTestId('productCard100').click();
+
+        await expect((page).getByText(/product/)).toBeVisible();
+
+        await page.getByTestId('addToCartButton').click();
+
+        await page.goto('/cart');
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-cart-${i}`),
+        });
+      })
+    )
+  );
+
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} order page ${i}`, async ({ page, port }) => {
+        await page.goto('/products');
+
+        await page.getByTestId('productCard100').click();
+
+        await expect((page).getByText(/product/)).toBeVisible();
+
+        await page.getByTestId('addToCartButton').click();
+
+        await page.goto('/cart');
+
+        await page.getByTestId('firstNameInput').fill('Touko');
+        await page.getByTestId('lastNameInput').fill('Lonka');
+        await page.getByTestId('addressInput').fill('Address 123');
+
+        await page.getByTestId('placeOrderButton').click();
+
+        await page.getByTestId('confirmButton').click();
+
+        await expect((page).getByText(/Order on/)).toBeVisible();
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-order-${i}`),
+        });
+      })
+    )
+  );
+
+  Array.from({ length: 5 }, (_, i) => i + 1).forEach((i) =>
+    names.forEach((name) =>
+      lighthouseTest(`${name} orders page ${i}`, async ({ page, port }) => {
+        await page.goto('/products');
+
+        await page.getByTestId('productCard100').click();
+
+        await expect((page).getByText(/product/)).toBeVisible();
+
+        await page.getByTestId('addToCartButton').click();
+
+        await page.goto('/cart');
+
+        await page.getByTestId('firstNameInput').fill('Touko');
+        await page.getByTestId('lastNameInput').fill('Lonka');
+        await page.getByTestId('addressInput').fill('Address 123');
+
+        await page.getByTestId('placeOrderButton').click();
+
+        await page.getByTestId('confirmButton').click();
+
+        await expect((page).getByText(/Order on/)).toBeVisible();
+
+        await page.goto('/orders');
+
+        await expect((page).getByText(/Order on/)).toBeVisible();
+
+        await playAudit({
+          page,
+          port,
+          thresholds,
+          reports: getReportsConfiguration(`${name}-orders-${i}`),
+        });
+      })
+    )
+  );
+}
--- test-playwright/tests/utils.js
+++ test-playwright/tests/utils.js
@@ -1,5 +1,7 @@
 const { chromium } = require('playwright');
 const { test } = require('@playwright/test');
+const fs = require('fs');
+const glob = require('glob');
 
 function getRandomInt() {
   const min = 4000;
@@ -28,4 +30,116 @@ const lighthouseTest = test.extend({
   ],
 });
 
-module.exports = { lighthouseTest };
+function getReportsConfiguration(prefix) {
+  return {
+    formats: {
+      json: true,
+      html: true,
+    },
+    name: `${prefix}-audit`,
+    directory: 'reports',
+  };
+}
+
+function median(values) {
+  const amount = values.length;
+
+  if (amount % 2) {
+    // For example, length is 5 -> pick 2nd from a zero-indexed array
+    return values[Math.floor(amount / 2)];
+  }
+
+  // For example, length is 6 -> pick average of indices 2 and 3
+  return (
+    (values[Math.floor(amount / 2)] + values[Math.floor(amount / 2) - 1]) / 2
+  );
+}
+
+function average(values) {
+  const sum = values.reduce((a, b) => a + b, 0);
+
+  return sum / values.length;
+}
+
+function readAudits(pageType, auditType) {
+  const files = glob.sync('reports/' + pageType + '*-audit.json');
+
+  const audits = files.map((f) => fs.readFileSync(f)).map((d) => JSON.parse(d));
+
+  return audits.map((a) => a['audits'][auditType]['numericValue']);
+}
+
+function printTable() {
+  // Check the output JSON files for possible audits
+  const auditTypes = [
+    'first-contentful-paint',
+    'largest-contentful-paint',
+    'interactive',
+    'max-potential-fid',
+    'server-response-time',
+  ];
+  const calculatedRows = {
+    react: {},
+  };
+
+  auditTypes.forEach((auditType) => {
+    const reactValues = readAudits('react-', auditType);
+
+    calculatedRows.react[auditType] = {
+      firstRun: reactValues[0],
+      median: median(reactValues.slice(1)),
+      average: average(reactValues.slice(1)),
+    };
+  });
+
+  class Table {
+    constructor(name, fcp1, fcp2, fcp3, lcp1, lcp2, lcp3, tti1, tti2, tti3, fid1, fid2, fid3, srt1, srt2, srt3) {
+      this.name = name;
+      this.fcp1 = fcp1;
+      this.fcp_med = fcp2;
+      this.fcp_avg = fcp3;
+      this.lcp1 = lcp1;
+      this.lcp_med = lcp2;
+      this.lcp_avg = lcp3;
+      this.tti1 = tti1;
+      this.tti_med = tti2;
+      this.tti_avg = tti3;
+      this.fid1 = fid1;
+      this.fid_med = fid2;
+      this.fid_avg = fid3;
+      this.srt1 = srt1;
+      this.srt_med = srt2;
+      this.srt_avg = srt3;
+    }
+  }
+
+  function getTable(name, property) {
+    return new Table(
+      name,
+      Math.round(calculatedRows[property]['first-contentful-paint'].firstRun),
+      Math.round(calculatedRows[property]['first-contentful-paint'].median),
+      Math.round(calculatedRows[property]['first-contentful-paint'].average),
+      Math.round(calculatedRows[property]['largest-contentful-paint'].firstRun),
+      Math.round(calculatedRows[property]['largest-contentful-paint'].median),
+      Math.round(calculatedRows[property]['largest-contentful-paint'].average),
+      Math.round(calculatedRows[property]['interactive'].firstRun),
+      Math.round(calculatedRows[property]['interactive'].median),
+      Math.round(calculatedRows[property]['interactive'].average),
+      Math.round(calculatedRows[property]['max-potential-fid'].firstRun),
+      Math.round(calculatedRows[property]['max-potential-fid'].median),
+      Math.round(calculatedRows[property]['max-potential-fid'].average),
+      Math.round(calculatedRows[property]['server-response-time'].firstRun),
+      Math.round(calculatedRows[property]['server-response-time'].median),
+      Math.round(calculatedRows[property]['server-response-time'].average)
+    );
+  }
+
+  const rows = [
+    ['React', 'react'],
+  ];
+
+  // eslint-disable-next-line no-console
+  console.table(rows.map((row) => getTable(row[0], row[1])));
+}
+
+module.exports = { lighthouseTest, getReportsConfiguration, printTable };
