44eedf24d8d9fba8785b1eb4552576c6b9c89caa
Touko Lonka <touko.lonka@gmail.com>
2023-04-21 13:11:50 +0300

    Simplify Next applications

--- next-preact/components/CustomLink.jsx
+++ next-preact/components/CustomLink.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import classNames from 'classnames';
 
 function CustomLink({ href, children, dataTestId, ...props }) {
-  const pathname = usePathname();
-  const isActive = href !== '/' && pathname.startsWith(href) || pathname === href;
-
   return (
     <Link
       href={href}
       data-testid={dataTestId}
       {...props}
-      className={classNames('block py-2 pr-4 hover:scale-110', { 'text-blue-700': isActive }, { 'text-gray-700': !isActive })}
+      className={classNames('block py-2 pr-4 hover:scale-110 text-gray-700')}
     >
       {children}
     </Link>
